bc3f520e9e50b61cd5498368d00bfde07082b7ce
William Couture <wcouture17@gmail.com>
2025-06-25 17:17:36 -0400

    Creates framework for program details and modification page

--- obj/Debug/net9.0/HAOS-CMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/HAOS-CMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HAOS-CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5470f27091a7e6997987905d34c32bbb8fad9c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9bc28056f73890e0602874bfbcd582a9142b96")]
 [assembly: System.Reflection.AssemblyProductAttribute("HAOS-CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HAOS-CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
