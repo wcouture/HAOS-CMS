16da496f0412d3f6d57c4c1090556cb1d7926705
William Couture <wcouture17@gmail.com>
2025-07-09 11:12:07 -0400

    Updates exercies and completedworkout to include types

--- Models/Training/Exercise.cs
+++ Models/Training/Exercise.cs
@@ -2,10 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HAOS.Models.Training;
 
+public enum ExerciseType
+{
+    Strength = 1,
+    Endurance = 2
+}
+
 public class Exercise
 {
     [Key]
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? DemoUrl { get; set; }
+    public ExerciseType Type { get; set; }
 }
--- Models/User/CompletedWorkout.cs
+++ Models/User/CompletedWorkout.cs
@@ -5,4 +5,8 @@ public class CompletedWorkout
     public int Id { get; set; }
     public int UserId { get; set; }
     public int WorkoutId { get; set; }
+    public DateTime CompletedDate { get; set; }
+    public int WeightUsed { get; set; }
+    public int Duration { get; set; }
+
 }
